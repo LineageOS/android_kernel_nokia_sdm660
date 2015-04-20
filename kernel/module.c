1265bc7dd47efb74b372181545de2fa7db9ca3fc
flar2 <asegaert@gmail.com>
2015-04-20 00:10:54 -0400

    module: Allow wlan to load
    Signed-off-by: flar2 <asegaert@gmail.com>

--- kernel/module.c
+++ kernel/module.c
@@ -1295,6 +1295,10 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
+	/* Force wlan to load */
+	if (!strncmp("wlan", mod->name, 4))
+		return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
@@ -2889,6 +2893,9 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 	const char *modmagic = get_modinfo(info, "vermagic");
 	int err;
 
+	if(!strncmp("wlan", mod->name, 4))
+		goto end;
+
 	if (flags & MODULE_INIT_IGNORE_VERMAGIC)
 		modmagic = NULL;
 
@@ -2903,6 +2910,7 @@ static int check_modinfo(struct module *mod, struct load_info *info, int flags)
 		return -ENOEXEC;
 	}
 
+end:
 	if (!get_modinfo(info, "intree")) {
 		if (!test_taint(TAINT_OOT_MODULE))
 			pr_warn("%s: loading out-of-tree module taints kernel.\n",
