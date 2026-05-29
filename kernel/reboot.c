8c005e5243436f04c0d8e14f71dee55faead821c
Tuan Anh <tuan73176@gmail.com>
2026-05-29 07:28:08 +0000

    kernel: reboot: Import FIH changes
    [    1.477937] fih_apr_property: rebootreason = 0x00000027
    
    Without this changes, reboot reason will always be 0x27, which is
    FIH_RERE_UNKNOWN_RESET. Since the bootloader saw an unknown reset reason,
    it assumed the device did not shut down cleanly and boot to Android instead
    of going to offline charging mode.
    
    For a clean power off, the reboot reason must be FIH_RERE_KERNEL_SHUTDOWN
    (0x23), so the bootloader will know how to boot into offline charging mode
    when plugged in later.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/work_items/10227
    Change-Id: I4ceb7098e046d94d6e8e2d096d73e0686f758b94

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -17,6 +17,10 @@
 #include <linux/syscore_ops.h>
 #include <linux/uaccess.h>
 
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+#include "../drivers/platform/fih/fih_rere.h"
+#endif
+
 /*
  * this indicates whether you can reboot with ctrl-alt-del: the default is yes
  */
@@ -67,6 +71,13 @@ EXPORT_SYMBOL_GPL(emergency_restart);
 
 void kernel_restart_prepare(char *cmd)
 {
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+	/* FIH, to support fih apr { */
+	fih_rere_wt_imem(FIH_RERE_KERNEL_RESTART);
+	pr_info("%s: rere = 0x%08x\n", __func__, fih_rere_rd_imem());
+	/* FIH, to support fih apr } */
+#endif
+
 	blocking_notifier_call_chain(&reboot_notifier_list, SYS_RESTART, cmd);
 	system_state = SYSTEM_RESTART;
 	usermodehelper_disable();
@@ -227,6 +238,13 @@ EXPORT_SYMBOL_GPL(kernel_restart);
 
 static void kernel_shutdown_prepare(enum system_states state)
 {
+#if defined(CONFIG_FIH_SDM630_SDM660_PROJS)
+	/* FIH, to support fih apr { */
+	fih_rere_wt_imem(FIH_RERE_KERNEL_SHUTDOWN);
+	pr_info("%s: rere = 0x%08x\n", __func__, fih_rere_rd_imem());
+	/* FIH, to support fih apr } */
+#endif
+
 	blocking_notifier_call_chain(&reboot_notifier_list,
 		(state == SYSTEM_HALT) ? SYS_HALT : SYS_POWER_OFF, NULL);
 	system_state = state;
